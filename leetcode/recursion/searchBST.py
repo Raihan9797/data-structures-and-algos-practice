42610f4648edb58b1d7b4737ccc3af0e59d80d27
Raihan9797 <raihanmuhd97@gmail.com>
2021-08-21 21:09:21 +0800

    searchBST solution

---
+++ leetcode/recursion/searchBST.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+
+
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        # base case: empty root -> return root
+        if not root:
+            return root
+        
+        # base case 2: we find the val at the root! -> return root
+        elif val == root.val:
+            return root
+        else:
+            
+            # val is less than root -> check left side
+            if val < root.val:
+                left_tree = Solution().searchBST(root.left, val)
+                return left_tree
+            
+            # val is more than root -> check right side
+            else:
+                right_tree = Solution().searchBST(root.right, val)
+                return right_tree
